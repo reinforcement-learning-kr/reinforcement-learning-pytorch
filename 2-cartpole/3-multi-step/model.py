204fe14ff13f41a053e072b85773122965b39073
dnddnjs <dnddnjs11@naver.com>
2018-10-06 11:35:05 +0900

    add multi step actor critic

---
+++ 2-cartpole/3-multi-step/model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(Model, self).__init__()
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        
+        self.fc1 = nn.Linear(num_inputs, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc_actor = nn.Linear(128, num_outputs)
+        self.fc_critic = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        policy = F.softmax(self.fc_actor(x))
+        value = self.fc_critic(x)
+        return policy, value
