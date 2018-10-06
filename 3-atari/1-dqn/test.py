db18dccd796e3d4ec56e9ea3298a3a55e5a2cfc9
dnddnjs <dnddnjs11@naver.com>
2018-10-06 15:58:01 +0900

    add atari dqn agent

---
+++ 3-atari/1-dqn/test.py
@@ -0,0 +1,61 @@
+import os
+import gym
+import random
+import argparse
+import numpy as np
+
+import torch
+from model import Model
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env_name', type=str, default="CartPole-v1", help='')
+parser.add_argument('--load_model', type=str, default=None)
+parser.add_argument('--save_path', default='./save_model/', help='')
+args = parser.parse_args()
+
+
+def get_action(qvalue):
+    _, action = torch.max(qvalue, 1)
+    return action.numpy()[0]
+
+
+if __name__=="__main__":
+    env = gym.make(args.env_name)
+    env.seed(500)
+    torch.manual_seed(500)
+
+    num_inputs = env.observation_space.shape[0]
+    num_actions = env.action_space.n
+    print('state size:', num_inputs)
+    print('action size:', num_actions)
+
+    net = Model(num_inputs, num_actions)
+    net.load_state_dict(torch.load(args.save_path + 'model.pth'))
+    
+    net.eval()
+    running_score = 0
+    steps = 0
+    
+    for e in range(5):
+        done = False
+        
+        score = 0
+        state = env.reset()
+        state = torch.Tensor(state)
+        state = state.unsqueeze(0)
+
+        while not done:
+            env.render()
+
+            steps += 1
+            qvalue = net(state)
+            action = get_action(qvalue)
+            next_state, reward, done, _ = env.step(action)
+            
+            next_state = torch.Tensor(next_state)
+            next_state = next_state.unsqueeze(0)
+
+            score += reward
+            state = next_state
+
+        print('{} episode | score: {:.2f}'.format(e, score))
---
+++ 3-atari/1-dqn/train.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import gym
+import random
+import argparse
+import numpy as np
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from utils import *
+from model import Model
+from memory import Memory
+from tensorboardX import SummaryWriter
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env_name', type=str, default="Breakout-v4", help='')
+parser.add_argument('--load_model', type=str, default=None)
+parser.add_argument('--save_path', default='./save_model/', help='')
+parser.add_argument('--render', default=False, action="store_true")
+parser.add_argument('--gamma', default=0.99, help='')
+parser.add_argument('--batch_size', default=32, help='')
+parser.add_argument('--initial_exploration', default=50000, help='')
+parser.add_argument('--update_target', default=10000, help='')
+parser.add_argument('--log_interval', default=1, help='')
+parser.add_argument('--goal_score', default=400, help='')
+parser.add_argument('--logdir', type=str, default='./logs',
+                    help='tensorboardx logs directory')
+args = parser.parse_args()
+
+
+def train_model(batch):
+    history = torch.stack(batch.history)
+    next_history = torch.stack(batch.next_history)
+    actions = torch.Tensor(batch.action).long()
+    rewards = torch.Tensor(batch.reward)
+    masks = torch.Tensor(batch.mask)
+
+    pred = net(history).squeeze(1)
+    next_pred = target_net(next_history).squeeze(1)
+    one_hot_action = torch.zeros(args.batch_size, pred.size(-1))
+    one_hot_action.scatter_(1, actions.unsqueeze(1), 1)
+    pred = torch.sum(pred.mul(one_hot_action), dim=1)
+
+    target = rewards + masks * args.gamma * next_pred.max(1)[0]
+    
+    loss = F.mse_loss(pred, target.detach())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+if __name__=="__main__":
+    env = gym.make(args.env_name)
+    env.seed(500)
+    torch.manual_seed(500)
+
+    img_shape = env.observation_space.shape
+    num_actions = 3
+    print('image size:', img_shape)
+    print('action size:', num_actions)
+
+    net = Model(num_actions)
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            torch.nn.init.xavier_uniform(m.weight)
+
+    net.apply(weights_init)
+    target_net = Model(num_actions)
+    update_target_model(net, target_net)
+
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    writer = SummaryWriter('logs')
+
+    if not os.path.isdir(args.save_path):
+        os.makedirs(args.save_path)
+    
+    net.train()
+    target_net.train()
+    memory = Memory(400000)
+    running_score = 0
+    epsilon = 1.0
+    steps = 0
+    
+    for e in range(10000):
+        done = False
+        dead = False
+
+        score = 0
+        start_life = 5
+        state = env.reset()
+
+        state = pre_process(state)
+        state = torch.Tensor(state)
+        history = torch.stack((state, state, state, state))
+
+        for i in range(3):
+            action = env.action_space.sample()
+            state, reward, done, info = env.step(action)
+            state = pre_process(state)
+            state = torch.Tensor(state)
+            state = state.unsqueeze(0)
+            history = torch.cat((state, history[1:]), dim=0)
+
+        while not done:
+            if args.render:
+                env.render()
+
+            steps += 1
+            qvalue = net(history.unsqueeze(0))
+            action = get_action(epsilon, qvalue, num_actions)
+            
+            if action == 0:
+                real_action = 1
+            elif action == 1:
+                real_action = 2
+            else:
+                real_action = 3
+
+            next_state, reward, done, info = env.step(real_action)
+            
+            next_state = pre_process(next_state)
+            next_state = torch.Tensor(next_state)
+            next_state = next_state.unsqueeze(0)
+            next_history = torch.cat((state, history[1:]), dim=0)
+
+            if start_life > info['ale.lives']:
+                dead = True
+                start_life = info['ale.lives']
+            
+            score += reward
+            reward = np.clip(reward, -1, 1)
+
+            mask = 0 if dead else 1
+            memory.push(history, next_history, action, reward, mask)
+            
+            if dead:
+                dead = False
+            else:
+                history = next_history
+
+            if steps > args.initial_exploration:
+                epsilon -= 1e-6
+                epsilon = max(epsilon, 0.1)
+
+                batch = memory.sample(args.batch_size)
+                train_model(batch)
+
+                if steps % args.update_target:
+                    update_target_model(net, target_net)
+
+
+        if e % args.log_interval == 0:
+            print('{} episode | score: {:.2f} | epsilon: {:.4f} | steps: {}'.format(
+                e, score, epsilon, steps))
+            writer.add_scalar('log/score', float(score), score)
+            writer.add_scalar('log/step', float(steps), steps)
+
+        if score > args.goal_score:
+            ckpt_path = args.save_path + 'model.pth'
+            torch.save(net.state_dict(), ckpt_path)
+            print('running score exceeds 400 so end')
+            break
